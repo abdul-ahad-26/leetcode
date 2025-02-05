b9ae44bf041c40742b51595fa7f03285863c7e14
abdul-ahad-26 <hayymalik678@gmail.com>
2025-02-05 23:41:47 +0500

    fix >

--- factorial.py
+++ factorial.py
@@ -1,10 +1,10 @@
 def fact(n):
     if n >=0 :
-           if n >= 1 or n==0:
+           if n == 1 or n==0:
                   return 1
            factorial=n*fact(n-1)
            return factorial
     else:
          return"value is negative"
                   
-print(fact(0))
+print(fact(4))
